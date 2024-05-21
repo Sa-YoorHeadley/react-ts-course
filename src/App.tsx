4cc4259a3354b5b86389446641c60d6f4be3cfaf
unknown <sai.h152015@gmail.com>
2024-05-21 15:32:29 -0400

    Finished reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import Counter from "./components/Counter";
-import Heading from "./components/Heading";
-import List from "./components/List";
-import Section from "./components/Section";
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState<number>(1);
 
   return (
     <>
-      <Heading title={"Hello World!"} />
-      <Section title={"Different Title"}>This is my section.</Section>
-      <Counter setCount={setCount}>Count is {count}</Counter>
-      <List
-        items={["☕ Coffee", "🌮 Tacos", "💻 Code"]}
-        render={(item: string) => <span className="gold bold">{item}</span>}
-      />
+      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
     </>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,56 @@
-import { ReactNode } from "react";
+import { ChangeEvent, ReactNode, useReducer } from "react";
 
-type CounterProps = {
-    setCount: React.Dispatch<React.SetStateAction<number>>,
-    children: ReactNode
+const initState = { count: 0, text: "" };
+
+const enum REDUCER_ACTION_TYPE {
+  INCREMENT,
+  DECREMENT,
+  NEW_INPUT,
 }
 
-function Counter({setCount, children} : CounterProps) {
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE;
+  payload?: string;
+};
+
+const reducer = (
+  state: typeof initState,
+  action: ReducerAction
+): typeof initState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case REDUCER_ACTION_TYPE.NEW_INPUT:
+      return { ...state, text: action.payload ?? "" };
+    default:
+      throw new Error();
+  }
+};
+
+type CounterProps = {
+  children: (num: number) => ReactNode;
+};
+
+function Counter({ children }: CounterProps) {
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT });
+  const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT });
+  const handleTextInput = (e: ChangeEvent<HTMLInputElement>) =>
+    dispatch({
+      type: REDUCER_ACTION_TYPE.NEW_INPUT,
+      payload: e.target.value ?? "",
+    });
+
   return (
     <>
-      <h1>{children}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+      <h1>{children(state.count)}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <input type="text" onChange={handleTextInput} />
+      <h2>{state.text}</h2>
     </>
   );
 }
