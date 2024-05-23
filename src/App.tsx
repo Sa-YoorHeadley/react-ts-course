e7f0200f8a83e9004cd55a2f3ba2d968fc971f39
unknown <sai.h152015@gmail.com>
2024-05-23 13:39:25 -0400

    Finished Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Counter from "./components/Counter";
+import { initState, CounterProvider } from "./context/CounterContext";
 
 function App() {
-
   return (
     <>
-      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      <CounterProvider count={initState.count} text={initState.text}>
+        <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      </CounterProvider>
     </>
   );
 }
